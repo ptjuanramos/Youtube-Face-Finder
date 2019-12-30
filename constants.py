cec16e32cbf791be0e59c32ad54358d2e9c76de0
Juan Ramos <ptjuanramos@gmail.com>
2019-12-30 10:53:36 +0000

    Creating read_multiple_mages function to read images for a given directory. Testing face_recognition api.

---
+++ constants.py
@@ -0,0 +1,2 @@
+KNOWN_PEOPLE_DIR = "known_people"
+UNKNOWN_PEOPLE_DIR = "unknown_people"
--- youtube-face-finder.py
+++ youtube-face-finder.py
@@ -1,27 +1,64 @@
 import sys
+import os
 from os import path
 import face_recognition
+import constants
 
+"""
+Check if image is .jpg extension(for now)
+"""
+def is_authorized_image_file(image_path):
+    filename, file_extension = path.splitext(image_path)
+    return file_extension == ".jpg" #For now is only jpg
+
+"""
+Reads image from the given path
+"""
 def read_image(image_path):
     if path.exists(image_path) == False:
         print("There is not such file with that path: {}".format(image_path))
         sys.exit()
 
-    filename, file_extension = path.splitext(image_path)
-    if file_extension != "jpg": #For now is only jpg
-        print("{} needs to be a jpg".format(filename))
+    if is_authorized_image_file(image_path) == False: #For now is only jpg
+        print("{} needs to be a jpg".format(image_path))
         sys.exit()
 
-    image = face_recognition.load_image_file(image_path)
+    return face_recognition.load_image_file(image_path)
 
-    return image
+"""
+Reads multiple images from the given directory path.
+Uses read_image(...)
+"""
+def read_multiple_images(dir_path):
+    if(path.exists(dir_path) == False):
+        print("There is not such directory with that path: {}".format(dir_path))
+        sys.exit()
+    
+    all_image_files_paths = [f for f in os.listdir(dir_path) if path.isfile(path.join(dir_path, f)) and is_authorized_image_file(f)]
+
+    images_content_list = []
+    for image_file_path in all_image_files_paths:
+        new_image_content = read_image(dir_path + "/" + image_file_path)
+        images_content_list.append(new_image_content)
+
+    return images_content_list
+
+
+#def find_face(known_image, unknown_image_to_eval):
 
 def main():
-    if len(sys.argv) != 2:
-        print("Please provide the image path as argument")
-        sys.exit()
+    """
+    unknown_image_content = read_image(constants.UNKNOWN_PEOPLE_DIR + "/picture_example1.jpg") #This will be a youtube stream
+    known_image_content = read_image(constants.KNOWN_PEOPLE_DIR + "/me_1.jpg")
 
-    image_content = read_image(sys.argv[1])
+    face_locations = face_recognition.face_locations(unknown_image_content)
+    print(face_locations)
+    print("How many people are in this image: {}".format(len(face_locations)))
+    """
+    all_dir_images_content = read_multiple_images(constants.UNKNOWN_PEOPLE_DIR)
+    for image_content in all_dir_images_content:
+        face_locations = face_recognition.face_locations(image_content)
+        print(face_locations)
 
 if __name__ == "__main__":
     main()
